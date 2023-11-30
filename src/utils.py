647253abe7144e4740be38a9a822822130bf2014
noahhansson <hansson.noah@gmail.com>
2023-11-30 19:23:16 +0100

    Initial commit

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import os
+
+
+def read_input(file_name: str) -> list[str]:
+    """Function for navigating to data folder and reading input.
+
+    Args:
+        file_name (str): File name to read.
+
+    Returns:
+        list[str]: Returns the input where each row is a list element
+    """
+
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
+
+    input_file = os.path.join(os.getcwd(), "..", "input", f"{file_name}.txt")
+    with open(input_file, 'r') as file:
+        contents = [val.strip() for val in file.readlines()]
+
+    return contents
