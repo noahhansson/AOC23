c2a0e9febbf2995e126846e99958f880b86d0429
noahhansson <hansson.noah@gmail.com>
2023-12-01 07:02:39 +0100

    Day 1

--- src/day_1.py
+++ src/day_1.py
@@ -3,8 +3,6 @@ from utils import read_input
 inpt = read_input("1")
 
 def get_first_solution():
-
-
     s = 0
     for line in inpt:
         numbers = "".join([x for x in line if x.isnumeric()])
@@ -14,28 +12,26 @@ def get_first_solution():
 
 def get_second_solution():
     s = 0
+
+    int_map = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
     for line in inpt:
         line_fixed = ""
         for c in line:
             line_fixed += c
-            if line_fixed.endswith("one"):
-                line_fixed = line_fixed.replace("one", "1")
-            elif line_fixed.endswith("two"):
-                line_fixed = line_fixed.replace("two", "2")
-            elif line_fixed.endswith("three"):
-                line_fixed = line_fixed.replace("three", "3")
-            elif line_fixed.endswith("four"):
-                line_fixed = line_fixed.replace("four", "4")
-            elif line_fixed.endswith("five"):
-                line_fixed = line_fixed.replace("five", "5")
-            elif line_fixed.endswith("six"):
-                line_fixed = line_fixed.replace("six", "6")
-            elif line_fixed.endswith("seven"):
-                line_fixed = line_fixed.replace("seven", "7")
-            elif line_fixed.endswith("eight"):
-                line_fixed = line_fixed.replace("eight", "8")
-            elif line_fixed.endswith("nine"):
-                line_fixed = line_fixed.replace("nine", "9")
+            for key, value in int_map.items():
+                if line_fixed.endswith(key):
+                    line_fixed = line_fixed.replace(key, str(value))
 
         numbers = "".join([x for x in line_fixed if x.isnumeric()])
         s += int(numbers[0] + numbers[-1])
